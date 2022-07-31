385c9f355d133836bf851e65b476c43357a9384a
SapumalPriyashantha <sapumalpriyashantha2000@gmail.com>
2022-07-31 11:12:05 +0530

    create Admin Panel

---
+++ assets/js/CarController.js
@@ -0,0 +1,40 @@
+
+//Add Cars brand inside dropdown
+$("#dropdown_addCar").append('<option>' + 'Select Car Brand' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Suzuki Alto' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Suzuki Alto K10' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Suzuki Celerio' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Perodua (Daihatsu) Axia' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Toyota Prius C/ Aqua' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Toyota Corolla Axio/ NZE141' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Perodua Bezza Prime Sedan' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Toyota Allion NZT 260' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Toyota Axio NKR 165' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Toyota Premio' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'Mercedes' + '</option>');
+$("#dropdown_addCar").append('<option>' + 'BMW i8' + '</option>');
+
+//Add Car types inside dropdown
+$("#dropdown_carType").append('<option>' + 'Select Car Type' + '</option>');
+$("#dropdown_carType").append('<option>' + 'General' + '</option>');
+$("#dropdown_carType").append('<option>' + 'Premium' + '</option>');
+$("#dropdown_carType").append('<option>' + 'Luxury' + '</option>');
+
+//Add Car transmission inside dropdown
+$("#dropdown_carTransmission").append('<option>' + 'Select Car Transmission' + '</option>');
+$("#dropdown_carTransmission").append('<option>' + 'Manual' + '</option>');
+$("#dropdown_carTransmission").append('<option>' + 'Auto' + '</option>');
+
+//Add Car KM inside dropdown
+$("#dropdown_carKM").append('<option>' + 'Select Car KM' + '</option>');
+$("#dropdown_carKM").append('<option>' + '0' + '</option>');
+
+//Add Car Fuel Type inside dropdown
+$("#dropdown_carFuel").append('<option>' + 'Select Car Fuel Type' + '</option>');
+$("#dropdown_carFuel").append('<option>' + 'Petrol' + '</option>');
+$("#dropdown_carFuel").append('<option>' + 'Diesel' + '</option>');
+
+//Add Car Status inside dropdown
+$("#dropdown_carStatus").append('<option>' + 'Select Car Status' + '</option>');
+$("#dropdown_carStatus").append('<option>' + 'Available' + '</option>');
+$("#dropdown_carStatus").append('<option>' + 'UnAvailable' + '</option>');
